f8a031f14b7eb7261fd6663d19c28b28cf058640
HankinsL <63049718+HankinsL@users.noreply.github.com>
2025-12-01 12:48:09 +0800

    Update diy.sh

--- armsr/armv8/diy/diy.sh
+++ armsr/armv8/diy/diy.sh
@@ -8,7 +8,6 @@ git clone https://github.com/ophub/luci-app-amlogic --depth=1 clone/amlogic
 git clone https://github.com/vernesong/OpenClash --depth=1 clone/openclash
 
 # Update packages
-rm -rf feeds/luci/applications/luci-app-passwall
 rm -rf feeds/luci/applications/luci-app-openclash
 cp -rf clone/amlogic/luci-app-amlogic clone/openclash/luci-app-openclash feeds/luci/applications/
 
