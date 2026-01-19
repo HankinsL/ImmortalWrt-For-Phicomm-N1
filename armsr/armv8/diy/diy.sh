2c9631a0516e8b2c0de727b96863cede189c466e
HankinsL <63049718+HankinsL@users.noreply.github.com>
2026-01-19 18:30:20 +0800

    Update diy.sh

--- armsr/armv8/diy/diy.sh
+++ armsr/armv8/diy/diy.sh
@@ -5,10 +5,12 @@ patch -p1 -f < $(dirname "$0")/luci.patch
 
 # Add packages
 git clone https://github.com/ophub/luci-app-amlogic --depth=1 clone/amlogic
+git clone https://github.com/Openwrt-Passwall/openwrt-passwall-packages --depth=1 clone/passwall
 git clone https://github.com/Openwrt-Passwall/openwrt-passwall --depth=1 clone/passwall
 
 # Update packages
 rm -rf feeds/luci/applications/luci-app-passwall
+rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
 cp -rf clone/amlogic/luci-app-amlogic clone/passwall/luci-app-passwall feeds/luci/applications/
 
 # Clean packages
