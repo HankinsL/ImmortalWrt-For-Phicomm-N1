177b5073390add12cedc157c8ca26e55066df60c
HankinsL <63049718+HankinsL@users.noreply.github.com>
2026-02-20 17:58:13 +0800

    Update mk_s905d_n1.sh

--- armsr/armv8/N1/mk_s905d_n1.sh
+++ armsr/armv8/N1/mk_s905d_n1.sh
@@ -168,10 +168,11 @@ cd $TGT_ROOT
 copy_supplement_files
 extract_glibc_programs
 adjust_openssl_config
-adjust_openclash_config
 adjust_getty_config
 adjust_openssh_config
+use_xrayplug_replace_v2rayplug
 create_fstab_config
+adjust_mosdns_config
 patch_admin_status_index_html
 adjust_kernel_env
 copy_uboot_to_fs
