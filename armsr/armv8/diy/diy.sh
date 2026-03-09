01535f11931de9dcf625c7aa90d325e901de0efa
HankinsL <63049718+HankinsL@users.noreply.github.com>
2026-03-09 16:28:30 +0800

    Update diy.sh

--- armsr/armv8/diy/diy.sh
+++ armsr/armv8/diy/diy.sh
@@ -10,11 +10,11 @@ git clone https://github.com/vernesong/OpenClash --depth=1 clone/openclash
 git clone https://github.com/sbwml/luci-app-mosdns --single-branch --depth=1 clone/mosdns
 git clone https://github.com/sbwml/packages_lang_golang --single-branch --depth=1 clone/golang
 git clone https://github.com/sbwml/v2ray-geodata --depth=1 clone/v2ray-geodata
-git clone https://github.com/kongfl888/luci-app-adguardhome --depth=1 clone/adguardhome
+git clone https://github.com/w9315273/luci-app-adguardhome --depth=1 package/luci-app-adguardhome
 
 # Update packages
-rm -rf feeds/luci/applications/luci-app-passwall feeds/luci/applications/luci-app-openclash feeds/packages/adguardhome feeds/packages/lang/golang feeds/packages/net/mosdns feeds/packages/net/v2ray-geodata
-cp -rf clone/amlogic/luci-app-amlogic clone/passwall/luci-app-passwall clone/openclash/luci-app-openclash clone/mosdns/luci-app-mosdns clone/adguardhome/luci-app-adguardhome feeds/luci/applications/
+rm -rf feeds/luci/applications/luci-app-passwall feeds/luci/applications/luci-app-openclash feeds/packages/lang/golang feeds/packages/net/mosdns feeds/packages/net/v2ray-geodata
+cp -rf clone/amlogic/luci-app-amlogic clone/passwall/luci-app-passwall clone/openclash/luci-app-openclash clone/mosdns/luci-app-mosdns feeds/luci/applications/
 cp -rf clone/golang feeds/packages/lang/
 cp -rf clone/mosdns/mosdns clone/mosdns/v2dat clone/v2ray-geodata feeds/packages/net/
 sed -i 's/llvm=true/llvm=false/' feeds/packages/lang/rust/Makefile
