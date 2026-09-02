d60729c58fe6ffe1e1df2da568316a013041b158
HankinsL <63049718+HankinsL@users.noreply.github.com>
2026-09-03 02:54:34 +0800

    Update diy.sh

--- armsr/armv8/diy/diy.sh
+++ armsr/armv8/diy/diy.sh
@@ -18,6 +18,7 @@ rm -rf feeds/luci/applications/luci-app-passwall feeds/luci/applications/luci-ap
 cp -rf clone/amlogic/luci-app-amlogic clone/passwall/luci-app-passwall clone/openclash/luci-app-openclash clone/mosdns/luci-app-mosdns feeds/luci/applications/
 cp -rf clone/golang feeds/packages/lang/
 cp -rf clone/mosdns/mosdns clone/mosdns/geo2txt clone/v2ray-geodata feeds/packages/net/
+sed -i '/luci-app-attendedsysupgrade/d' feeds/luci/collections/luci/Makefile
 
 # Clean packages
 rm -rf clone
